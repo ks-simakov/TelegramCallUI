f42b5f72328a78f9e0c3413c3c18eeb0d42bf601
Konstantin Simakov <k@simakov.it>
2025-05-18 07:47:06 +0200

    Initial Commit

---
+++ TelegramCallUI/TelegramCallUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramCallUIApp.swift
+//  TelegramCallUI
+//
+//  Created by Konstantin Simakov on 18.05.25.
+//
+
+import SwiftUI
+
+@main
+struct TelegramCallUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
